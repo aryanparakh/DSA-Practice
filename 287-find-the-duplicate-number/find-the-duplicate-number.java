30cfcc11eeda301c498dc658a374217e6650e443
Aryan Parakh <Aryan.24BCS10336@sst.scaler.com>
2026-02-13 08:13:08 +0530

    Time: 22 ms (27.49%) | Memory: 83 MB (55.66%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int l = 1;
+        int r = nums.length - 1;
+         while (l < r) {
+            int mid = l + (r - l) / 2;
+             int count = 0;
+            for (int num : nums) {
+                if (num <= mid) count++;
+            }
+            
+            if (count > mid) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return l;
+        
+    }
+}
